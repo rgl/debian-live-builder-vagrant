cf859daa5bf1756d7a8ccc58615cbeb77574d232
Rui Lopes <rgl@ruilopes.com>
2019-01-04 23:13:25 +0000

    create the libvirt-vagrant empty box if needed

--- create_empty_box.sh
+++ create_empty_box.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 set -eu
 
-[ -d ~/.vagrant.d/boxes/empty ] && exit 0
-
-set -eux
-
 rm -rf tmp-empty-box 
 mkdir -p tmp-empty-box
 pushd tmp-empty-box
 
+# create and add an empty box to the virtualbox provider.
 TEMPLATE_BOX=~/.vagrant.d/boxes/debian-9-amd64/0/virtualbox
-
+if [ ! -d ~/.vagrant.d/boxes/empty/0/virtualbox ] && [ -d $TEMPLATE_BOX ]; then
 echo '{"provider":"virtualbox"}' >metadata.json
 VBoxManage createhd --filename empty.vmdk --format VMDK --size 10000
 sed -r \
-    -e 's,packer-debian-9-amd64-virtualbox-.+?.vmdk,empty.vmdk,' \
-    -e 's,([">]packer-debian-9-amd64-virtualbox-)[^"<]+?,\1empty,' \
+    -e 's,packer-.+-virtualbox-.+?.vmdk,empty.vmdk,' \
+    -e 's,([">]packer-.+-virtualbox-)[^"<]+?,\1empty,' \
     $TEMPLATE_BOX/box.ovf \
     >box.ovf
 cp $TEMPLATE_BOX/Vagrantfile .
 tar cvzf empty.box metadata.json Vagrantfile box.ovf empty.vmdk
 VBoxManage closemedium empty.vmdk --delete
+vagrant box add --force empty empty.box
+fi
 
-vagrant box add empty empty.box
+# create and add an empty box to the libvirt provider.
+TEMPLATE_BOX=~/.vagrant.d/boxes/debian-9-amd64/0/libvirt
+if [ ! -d ~/.vagrant.d/boxes/empty/0/libvirt ] && [ -d $TEMPLATE_BOX ]; then
+rm -f *
+cp $TEMPLATE_BOX/Vagrantfile .
+echo '{"format":"qcow2","provider":"libvirt","virtual_size":10}' >metadata.json
+qemu-img create -f qcow2 box.img 10G
+tar cvzf empty.box metadata.json Vagrantfile box.img
+vagrant box add --force empty empty.box
+fi
 
 popd
 rm -rf tmp-empty-box
